4113ffd84cf536d53aaa274b60bef49e3971cc09
Chandrakanth Ande <chandrakanth.ande@cgi.com>
2019-08-29 14:48:10 +0530

    Completed

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RNATranscriptionModule
 {
     public static class RnaTranscription
     {
         public static string ToRna(string nucleotide)
+           
         {
-            throw new NotImplementedException("You need to implement this function.");
+            string f = "";
+            Dictionary<char,char> rule = new Dictionary<char,char>();
+            rule.Add('G', 'C');
+            rule.Add('C', 'G');
+            rule.Add('T', 'A');
+            rule.Add('A', 'U');
+            for(int i=0;i<nucleotide.Length;i++)
+            {
+                f += rule[nucleotide[i]];
+            }
+            return f;
+            
+        }
+
+
+        public static void Main()
+        {
+
         }
     }
 }
